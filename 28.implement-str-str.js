9f8199d0a6eb0a009a843f09000dd9f7b7d6a9b2
yogendra saxena <yogendrasaxena@Admins-MacBook-Pro-4.local>
2019-10-09 14:17:37 +0530

    algo added

---
+++ 28.implement-str-str.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=28 lang=javascript
+ *
+ * [28] Implement strStr()
+ */
+
+// @lc code=start
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+  if (needle === "") return 0;
+  return haystack.indexOf(needle);
+};
+// @lc code=end
